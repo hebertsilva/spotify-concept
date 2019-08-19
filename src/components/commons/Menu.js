d7364c353d99d3f6662fdcaf47f1a307675fcc4b
Herbert Silva <hebertpereirasilva@gmail.com>
2019-08-19 11:10:22 -0300

    menu

--- src/components/commons/Menu.js
+++ src/components/commons/Menu.js
@@ -1,59 +1,66 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../../static/img/Spotify-logo.png'
 import './style.scss'
 
-export default function Menu() {
-  return (
-    <nav className="profile-sidebar">
-      <div className="sidebar-name">
-        <h1>
-          <img src={logo} alt="Spotify Concept" width="120" />
-          <p>Concept</p>
-        </h1>
-      </div>
+export default class Menu extends Component {
+  componentDidMount() {
+    const props = this.props
+    console.log(props)
+  }
 
-      <ol className="sidebar-nav">
-        <li>
-          <Link to="/me/playlits">
-            <span>
-              <i className="icon-music"></i>
-            </span>
-            Playlists
-            <i className="icon-angle-right"></i>
-          </Link>
-        </li>
+  render() {
+    return (
+      <nav className="profile-sidebar">
+        <div className="sidebar-name">
+          <h1>
+            <img src={logo} alt="Spotify Concept" width="120" />
+            <p>Concept</p>
+          </h1>
+        </div>
 
-        <li>
-          <a href="/">
-            <span>
-              <i className="icon-music"></i>
-            </span>
-            Albums
-            <i className="icon-angle-right"></i>
-          </a>
-        </li>
+        <ol className="sidebar-nav">
+          <li>
+            <Link to="/me/playlits">
+              <span>
+                <i className="icon-music"></i>
+              </span>
+              Playlists
+              <i className="icon-angle-right"></i>
+            </Link>
+          </li>
 
-        <li>
-          <a href="/">
-            <span>
-              <i className="icon-music"></i>
-            </span>
-            Playlist
-            <i className="icon-angle-right"></i>
-          </a>
-        </li>
+          <li>
+            <a href="/">
+              <span>
+                <i className="icon-music"></i>
+              </span>
+              Albums
+              <i className="icon-angle-right"></i>
+            </a>
+          </li>
 
-        <li>
-          <a href="/">
-            <span>
-              <i className="icon-music"></i>
-            </span>
-            Playlist
-            <i className="icon-angle-right"></i>
-          </a>
-        </li>
-      </ol>
-    </nav>
-  )
+          <li>
+            <a href="/">
+              <span>
+                <i className="icon-music"></i>
+              </span>
+              Playlist
+              <i className="icon-angle-right"></i>
+            </a>
+          </li>
+
+          <li>
+            <a href="/">
+              <span>
+                <i className="icon-music"></i>
+              </span>
+              Playlist
+              <i className="icon-angle-right"></i>
+            </a>
+          </li>
+        </ol>
+      </nav>
+    )
+  }
 }
