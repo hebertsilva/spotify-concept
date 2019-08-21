0e806f6c656e86a5cf3ccf38979f5e0883191694
Herbert Silva <hebertpereirasilva@gmail.com>
2019-08-21 15:51:17 -0300

    redis pass

--- src/server/redis.js
+++ src/server/redis.js
@@ -4,11 +4,13 @@ require('dotenv').config()
 
 const PORT = process.env.REDIS_PORT || 6379
 const HOST = process.env.REDIS_HOST || 'localhost'
+const REDIS_PASS = process.env.REDIS_PASS || ''
 
 const client = redis.createClient({
   port: PORT,
   host: HOST,
-  db: 0
+  db: 0,
+  auth_pass: REDIS_PASS
 })
 
 client.on('error', err => {
